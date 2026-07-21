3b5aeda5c3f2a87d43e1b4aeef12b9de4cd0bc96
ovsisg <ovsisg@gmail.com>
2026-07-21 15:53:06 +0100

    Set up sqlite3 and added sample data to table

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,9 +20,22 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(5)
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Date of Birth", "Course", "Email"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)    
+        
+    def load_data(self):
+        conn = sqlite3.connect("database.db")
+        result = conn.execute("SELECT * FROM student")
+        self.table.setRowCount(0)
+        for row_index, row_data in enumerate(result):
+            self.table.insertRow(row_index)
+            for column_index, cell_data in enumerate(row_data):
+                self.table.setItem(row_index, column_index, QTableWidgetItem(str(cell_data)))
+                
+        conn.close()
 
 app = QApplication(sys.argv)   
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
